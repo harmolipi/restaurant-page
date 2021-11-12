7e6a0a58e7288dc5d949682308a3606cbfcf4eef
harmolipi <nikob381@gmail.com>
2021-11-12 13:19:19 -0500

    Formatted navbar and food items

--- src/page-load.js
+++ src/page-load.js
@@ -1,30 +1,44 @@
-// export default function loadPage() {
-//   console.log('loadPage running');
-//   const content = document.querySelector('#content');
-//   content.appendChild(createNavbar());
-// }
-
 const loadPage = () => {
   const content = document.querySelector('#content');
   content.appendChild(createNavbar());
   content.appendChild(createHero());
-  content.appendChild(createIntro());
-  content.appendChild(createFoodDisplay());
+  content.appendChild(createHomepage());
   content.appendChild(createFooter());
 }
 
 const createNavbar = () => {
   const navbar = document.createElement('nav');
-  navbar.classList.add('navbar');
+  navbar.classList.add('navbar', 'is-primary');
   const navbarBrand = document.createElement('div');
   navbarBrand.classList.add('navbar-brand');
-  const navbarBrandItem = document.createElement('div');
+  const navbarBrandItem = document.createElement('a');
   navbarBrandItem.classList.add('navbar-item');
-  navbarBrandItem.innerHTML = 'Good Eats Co.';
-  
+  const navbarTitle = document.createElement('h4');
+  navbarTitle.classList.add('title', 'is-4', 'has-text-white');
+  navbarTitle.innerHTML = 'Good Eats Co.';
+  const navbarMenu = document.createElement('div');
+  navbarMenu.classList.add('navbar-menu');
+  const navbarEnd = document.createElement('div');
+  navbarEnd.classList.add('navbar-end');
+
   navbar.appendChild(navbarBrand);
   navbarBrand.appendChild(navbarBrandItem);
+  navbarBrandItem.appendChild(navbarTitle);
+  navbar.appendChild(navbarMenu);
+  navbarMenu.appendChild(navbarEnd);
+  navbarEnd.appendChild(createNavbarLink('Gallery', 'gallery'));
+  navbarEnd.appendChild(createNavbarLink('Contact', 'contact'));
   return navbar;
+}
+
+const createNavbarLink = (text, id) => {
+  const navbarLink = document.createElement('a');
+  navbarLink.classList.add('navbar-item');
+  if(id) navbarLink.setAttribute('id', id);
+  const navbarLinkText = document.createElement('p');
+  navbarLinkText.innerHTML = text;
+  navbarLink.appendChild(navbarLinkText);
+  return navbarLink;
 }
 
 const createHero = () => {
@@ -37,7 +51,7 @@ const createHero = () => {
   heroTitle.innerHTML = 'Good Eats Co.';
   const heroSubtitle = document.createElement('h2');
   heroSubtitle.classList.add('subtitle', 'is-3');
-  heroSubtitle.innerHTML = '(Definitely up to code)';
+  heroSubtitle.innerHTML = '(Definitely up to health code)';
 
   hero.appendChild(heroBody);
   heroBody.appendChild(heroTitle);
@@ -56,36 +70,62 @@ const createIntro = () => {
 
 const createFoodDisplay = () => {
   const foodDisplay = document.createElement('div');
-  foodDisplay.classList.add('container');
+  foodDisplay.classList.add('container', 'block');
   const foodDisplaySection = document.createElement('section');
   foodDisplaySection.classList.add('food', 'tile', 'is-ancestor');
   
   foodDisplay.appendChild(foodDisplaySection);
-  foodDisplaySection.appendChild(createFoodItem('/src/souvlaki-meal.jpg'));
-  foodDisplaySection.appendChild(createFoodItem('/src/pastitsio.jpg'));
-  foodDisplaySection.appendChild(createFoodItem('/src/gyro.jpg'));
+  foodDisplaySection.appendChild(createFoodItem('/src/souvlaki-meal.jpg', 'Souvlaki'));
+  foodDisplaySection.appendChild(createFoodItem('/src/pastitsio.jpg', 'Pastitsio'));
+  foodDisplaySection.appendChild(createFoodItem('/src/gyro.jpg', 'Gyro'));
   return foodDisplay;
 }
 
-const createFoodItem = (picture) => {
+const createHomepage = () => {
+  const homepage = document.createElement('div');
+  homepage.setAttribute('id', 'homePage');
+  homepage.appendChild(createIntro());
+  homepage.appendChild(createFoodDisplay());
+  homepage.appendChild(createBlockSpacer());
+  return homepage;
+}
+
+const createFoodItem = (picture, label) => {
   const parentTile = document.createElement('div');
   parentTile.classList.add('tile', 'is-parent', 'is-4');
   const childTile = document.createElement('div');
-  childTile.classList.add('tile', 'is-child', 'box');
+  childTile.classList.add('tile', 'is-child', 'card');
+  const foodCardImageContainer = document.createElement('div');
+  foodCardImageContainer.classList.add('card-image');
   const foodImageContainer = document.createElement('figure');
-  foodImageContainer.classList.add('image');
+  foodImageContainer.classList.add('image', 'is-4by3');
   const foodImage = document.createElement('img');
   foodImage.src = picture;
-
+  
   parentTile.appendChild(childTile);
-  childTile.appendChild(foodImageContainer);
+  childTile.appendChild(foodCardImageContainer);
+  foodCardImageContainer.appendChild(foodImageContainer);
   foodImageContainer.appendChild(foodImage);
+
+  if(label) {
+    const foodNameLabel = document.createElement('div');
+    foodNameLabel.classList.add('food-name-label', 'card-content');
+    const foodName = document.createElement('p');
+    foodName.classList.add('has-text-centered', 'is-size-4');
+    foodName.innerHTML = label;
+    childTile.appendChild(foodNameLabel);
+    foodNameLabel.appendChild(foodName);
+  }
   return parentTile;
 }
 
+const createBlockSpacer = () => {
+  const blockSpacer = document.createElement('div');
+  blockSpacer.classList.add('block');
+  return blockSpacer;
+}
+
 const createFooter = () => {
-  const footerContainer = document.createElement('div');
-  footerContainer.classList.add('container');
   const footer = document.createElement('footer');
   footer.classList.add('footer');
   const footerContent = document.createElement('div');
@@ -95,11 +135,10 @@ const createFooter = () => {
   const footerTextName = document.createElement('p');
   footerTextName.innerHTML = 'Created by: <a href="https://www.github.com/harmolipi" target="_blank">@harmolipi</a>';
   
-  footerContainer.appendChild(footer);
   footer.appendChild(footerContent);
   footerContent.appendChild(footerText);
   footerContent.appendChild(footerTextName);
-  return footerContainer;
+  return footer;
 }
 
 export default loadPage;
